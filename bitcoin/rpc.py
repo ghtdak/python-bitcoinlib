e178859cd8f7b0df7a299e77cf3a56bcaa3b1ce8
Peter Todd <pete@petertodd.org>
2013-08-28 01:03:14 -0400

    Fix bitcoin.rpc Python3 incompatibility
     [ yapified by gitreformat (github/ghtdak) on Mon Nov 30 21:07:25 2015 ]

--- bitcoin/rpc.py
+++ bitcoin/rpc.py
@@ -74,7 +74,7 @@ class AuthServiceProxy(object):
         self.__id_count = 0
         authpair = "%s:%s" % (self.__url.username, self.__url.password)
         authpair = authpair.encode('utf8')
-        self.__auth_header = "Basic %s" % base64.b64encode(authpair)
+        self.__auth_header = b"Basic " + base64.b64encode(authpair)
 
         if connection:
             # Callables re-use the connection of the original proxy
