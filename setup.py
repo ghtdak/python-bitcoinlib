63493bbd9fe91e40819ca1533a604d1d54d5e4d3
Peter Todd <pete@petertodd.org>
2013-11-04 05:57:21 -0500

    Change setup.py version to 0.1
    Lets keep expectations reasonable...
    
     [ yapified by gitreformat (github/ghtdak) on Mon Nov 30 21:08:12 2015 ]

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = []
 
 setup(
     name='python-bitcoinlib',
-    version='1.0',
+    version='0.1',
     description=
     'This python library provides an easy interface to the bitcoin data structures and protocol.',
     long_description=README,
