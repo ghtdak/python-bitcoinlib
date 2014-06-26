d5df1f549d8e6d873b1f16241d3f13f6341760fc
Derren Desouza <derrend@yahoo.co.uk>
2014-06-26 09:11:33 +0100

    Update bip-0070-payment-protocol.py
    Added some urllib2 code
     [ yapified by gitreformat (github/ghtdak) on Mon Nov 30 21:09:56 2015 ]

--- examples/bip-0070-payment-protocol.py
+++ examples/bip-0070-payment-protocol.py
@@ -11,6 +11,8 @@ Handles incoming serialized string data in the form of a http request
 and returns an appropriate response using googles protocol buffers.
 """
 
+import urllib2
+
 # https://github.com/bitcoin/bips/blob/master/bip-0070/paymentrequest.proto
 import payments_pb2
 o = payments_pb2
@@ -47,8 +49,14 @@ def payment_request(request):
     pro = o.PaymentRequest()
     pro.serialized_payment_details = pdo.SerializeToString()
 
-    return HttpResponse(pro.SerializeToString(),
-                        content_type="application/bitcoin-paymentrequest")
+    sds_pr = pro.SerializeToString()
+
+    open('sds_pr_blob', 'wb').write(sds_pr)
+    headers = {'Content-Type': 'application/bitcoin-payment',
+               'Accept': 'application/bitcoin-paymentrequest'}
+    response = urllib2.Request('file:sds_prblob', None, headers)
+
+    return urllib2.urlrequest(response.read())
 
 
 def payment_ack(request):
@@ -61,5 +69,11 @@ def payment_ack(request):
     refund_address = CBitcoinAddress.from_scriptPubKey(CScript(
         pao.payment.refund_to[0].script))
 
-    return HttpResponse(pao.SerializeToString(),
-                        content_type="application/bitcoin-paymentack")
+    sds_pa = pao.SerializeToString()
+
+    open('sds_pablob', 'wb').write(sds_pa)
+    headers = {'Content-Type': 'application/bitcoin-payment',
+               'Accept': 'application/bitcoin-paymentack'}
+    response = urllib2.Request('file:sds_pablob', None, headers)
+
+    return urllib2.urlrequest(response.read())
