4355459e9a1acb73655c83e891c341a6ed57da82
Peter Todd <pete@petertodd.org>
2014-08-24 21:34:44 -0400

    Release v0.2.0
     [ yapified by gitreformat (github/ghtdak) on Mon Nov 30 21:10:14 2015 ]

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = []
 
 setup(
     name='python-bitcoinlib',
-    version='0.2-SNAPSHOT',
+    version='0.2.0',
     description=
     'This python library provides an easy interface to the bitcoin data structures and protocol.',
     long_description=README,
