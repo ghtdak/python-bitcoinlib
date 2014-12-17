5d1829ab6d3137b6e4f3f8ce4980c1e02303ecf2
Peter Todd <pete@petertodd.org>
2014-12-17 03:24:48 -0500

    Use explicit import and __all__ in bitcoin.net
    Previously was exporting bitcoin.core.serialize namespace.
    
     [ yapified by gitreformat (github/ghtdak) on Mon Nov 30 21:11:11 2015 ]

--- bitcoin/net.py
+++ bitcoin/net.py
@@ -15,8 +15,10 @@ import struct
 import socket
 from binascii import hexlify
 
-from .core import PROTO_VERSION, CADDR_TIME_VERSION
-from .core.serialize import *
+from bitcoin.core.serialize import (Serializable,
+                                    VarStringSerializer,
+                                    ser_read,
+                                    uint256VectorSerializer,)
 
 PROTO_VERSION = 60002
 CADDR_TIME_VERSION = 31402
@@ -178,3 +180,12 @@ class CAlert(Serializable):
     def __repr__(self):
         return "CAlert(vchMsg.sz %d, vchSig.sz %d)" % (len(self.vchMsg),
                                                        len(self.vchSig))
+
+
+__all__ = ('PROTO_VERSION',
+           'CADDR_TIME_VERSION',
+           'CAddress',
+           'CInv',
+           'CBlockLocator',
+           'CUnsignedAlert',
+           'CAlert',)
