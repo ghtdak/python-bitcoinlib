11153c84df7a142a920c214c7ffe3a30fb04451a
Peter Todd <pete@petertodd.org>
2014-12-17 03:12:19 -0500

    Make ISA_BINOP and ISA_UNOP private
    These are really implementation-specific details; not sure we want to
    export them.
    
     [ yapified by gitreformat (github/ghtdak) on Mon Nov 30 21:11:09 2015 ]

--- bitcoin/core/scripteval.py
+++ bitcoin/core/scripteval.py
@@ -218,7 +218,7 @@ def _CheckMultiSig(opcode, script, stack, txTo, inIdx, err_raiser, nOpCount):
             stack.append(b"\x00")
 
 # OP_2MUL and OP_2DIV are *not* included in this list as they are disabled
-ISA_UNOP = {OP_1ADD, OP_1SUB, OP_NEGATE, OP_ABS, OP_NOT, OP_0NOTEQUAL,}
+_ISA_UNOP = {OP_1ADD, OP_1SUB, OP_NEGATE, OP_ABS, OP_NOT, OP_0NOTEQUAL,}
 
 
 def _UnaryOp(opcode, stack, err_raiser):
@@ -253,7 +253,7 @@ def _UnaryOp(opcode, stack, err_raiser):
     stack.append(bitcoin.core.bignum.bn2vch(bn))
 
 # OP_LSHIFT and OP_RSHIFT are *not* included in this list as they are disabled
-ISA_BINOP = {
+_ISA_BINOP = {
     OP_ADD,
     OP_SUB,
     OP_BOOLAND,
@@ -419,10 +419,10 @@ def _EvalScript(stack, scriptIn, txTo, inIdx, flags=()):
                 v = sop - (OP_1 - 1)
                 stack.append(bitcoin.core.bignum.bn2vch(v))
 
-            elif sop in ISA_BINOP:
+            elif sop in _ISA_BINOP:
                 _BinOp(sop, stack, err_raiser)
 
-            elif sop in ISA_UNOP:
+            elif sop in _ISA_UNOP:
                 _UnaryOp(sop, stack, err_raiser)
 
             elif sop == OP_2DROP:
