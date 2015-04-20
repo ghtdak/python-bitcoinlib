86c8681cf120676ee410245add79deba6d2a5706
Peter Todd <pete@petertodd.org>
2015-04-20 02:05:32 -0400

    Release v0.4.0
     [ yapified by gitreformat (github/ghtdak) on Mon Nov 30 21:12:10 2015 ]

--- setup.py
+++ setup.py
@@ -9,18 +9,16 @@ with open(os.path.join(here, 'README')) as f:
 
 requires = []
 
-setup(
-    name='python-bitcoinlib',
-    version='0.3.1-SNAPSHOT',
-    description=
-    'This python library provides an easy interface to the Bitcoin data structures and protocol.',
-    long_description=README,
-    classifiers=[
-        "Programming Language :: Python",
-    ],
-    url='https://github.com/petertodd/python-bitcoinlib',
-    keywords='bitcoin',
-    packages=find_packages(),
-    zip_safe=False,
-    install_requires=requires,
-    test_suite="bitcoin.tests")
+setup(name='python-bitcoinlib',
+      version='0.4.0',
+      description='The Swiss Army Knife of the Bitcoin protocol.',
+      long_description=README,
+      classifiers=[
+          "Programming Language :: Python",
+      ],
+      url='https://github.com/petertodd/python-bitcoinlib',
+      keywords='bitcoin',
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=requires,
+      test_suite="bitcoin.tests")
